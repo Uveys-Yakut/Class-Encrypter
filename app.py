3c4017e35b1139aa1b74b3fd43f1550500e0e1af
Uveys <120497074+Uveys-Yakut@users.noreply.github.com>
2024-09-16 21:06:49 +0300

    Modified

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from api import api_blueprint
 from flask import Flask, render_template
 from flask_cors import CORS
@@ -15,22 +14,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-=======
-from api import api_blueprint
-from flask import Flask, render_template
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
-
-# API Blueprint'ini uygulamaya ekle
-app.register_blueprint(api_blueprint, url_prefix='/api')
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-if __name__ == "__main__":
->>>>>>> 40f5801 (Update App)
     app.run(debug=True)
